1654e2aaca43f329edefd3e81c66fa10b30f77c0
haroonshinwari55 <haroonshinwarimessi@gamil.com>
2026-07-15 17:20:07 -0700

    update project

--- backend/src/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cvbuilder';
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
-};
